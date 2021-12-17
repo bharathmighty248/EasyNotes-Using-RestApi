07eb3891b191822f486ce027304a296c9bc41f41
bharathmighty248 <bharathmighty248@gmail.com>
2021-12-17 18:10:13 +0530

    Feat: Configuring and Connecting to the database

--- server.js
+++ server.js
@@ -1,6 +1,19 @@
 const express = require('express');
+const dbConfig = require('./config/database.config');
+const mongoose = require ('mongoose');
 
 const app = express();
+
+mongoose.Promise = global.Promise;
+
+mongoose.connect(dbConfig.url, {
+    useNewUrlParser: true
+}).then(() => {
+    console.log("Successfully connected to the database");    
+}).catch(err => {
+    console.log('Could not connect to the database. Exiting now...', err);
+    process.exit();
+});
 
 app.get('/',(req, res) => {
     res.json({"message": "Welcome to EasyNotes application. Take notes quickly. Organize and keep track of all your notes."})
