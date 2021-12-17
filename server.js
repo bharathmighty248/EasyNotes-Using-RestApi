d9888beef50f60b7c1f7b3960d2f04d4ac0c80b3
bharathmighty248 <bharathmighty248@gmail.com>
2021-12-17 19:02:59 +0530

    Feat: Writing the Controller functions for CRUD operations of Note

--- server.js
+++ server.js
@@ -1,8 +1,13 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 const dbConfig = require('./config/database.config');
 const mongoose = require ('mongoose');
 
 const app = express();
+
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.use(bodyParser.json());
 
 mongoose.Promise = global.Promise;
 
